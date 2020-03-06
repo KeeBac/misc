1eb0e42b1ad406b9a58091cc3745bcc1be674ef3
KeeBac <32729601+KeeBac@users.noreply.github.com>
2020-03-06 19:16:21 +0530

    Add files via upload

---
+++ tracking.py
@@ -0,0 +1,36 @@
+import mlflow
+import os
+
+MLFLOW_TRACKING_URL = 'http://13.79.158.10:5000' #os.getenv('MLFLOW_TRACKING_URL')
+TENANT = os.getenv('TENANT','local')
+RUN_LABEL = os.getenv('GO_PIPELINE_LABEL', '0')
+USE_MLFLOW = MLFLOW_TRACKING_URL is not None
+
+class track:
+    def __enter__(self):
+        if USE_MLFLOW:
+            mlflow.set_tracking_uri(uri=MLFLOW_TRACKING_URL)
+            mlflow.set_experiment(TENANT)
+            mlflow.start_run(run_name=RUN_LABEL)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if USE_MLFLOW:
+            mlflow.end_run()
+
+    def set_model(self, model):
+        if USE_MLFLOW:
+            print('logging name',model.name)
+            mlflow.log_param('model', model.name)
+
+    def log_params(self, params):
+        if USE_MLFLOW:
+            for param in params:
+                print('logging param',param, params[param])
+                mlflow.log_param(param, params[param])
+
+    def log_metrics(self, metrics):
+        if USE_MLFLOW:
+            for metric in metrics:
+                print('metric',metric, metrics[metric])
+                mlflow.log_metric(metric, metrics[metric])
