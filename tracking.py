f1148aa843b108d6c17f0d89bc2d7fc35a5fda6f
KeeBac <32729601+KeeBac@users.noreply.github.com>
2020-03-11 15:17:13 +0530

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -1,7 +1,7 @@
 import mlflow
 import os
 
-MLFLOW_TRACKING_URL = os.getenv('MLFLOW_TRACKING_URL')# 'http://13.74.145.30:5000'  
+MLFLOW_TRACKING_URL = 'http://localhost:5000' #os.getenv('MLFLOW_TRACKING_URL')# 'http://13.74.145.30:5000'  
 TENANT = os.getenv('TENANT','local')
 RUN_LABEL = os.getenv('GO_PIPELINE_LABEL', '0')
 USE_MLFLOW = MLFLOW_TRACKING_URL is not None
