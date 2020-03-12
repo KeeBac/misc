52835946d701f69b18912119c987543594a1b20f
KeeBac <32729601+KeeBac@users.noreply.github.com>
2020-03-12 12:38:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 #run decision tree - check records in mlflow
-python /test_url/decision_tree.py
+#python /test_url/decision_tree.py
 
 #run fluentd - check records in kibana (test*)
-python /test_url/test_fluentd1.py
+#python /test_url/test_fluentd1.py
+
+#print env vars
+echo "printing env var..."
+echo "$MLFLOW_TRACKING_URL"
+
